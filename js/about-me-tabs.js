c8016e817ffdf9dc8f7934a1baa9454fd0503248
SamR0429 <Samanatha.ramos01@gmail.com>
2023-11-03 18:55:14 -0500

    fixed functionality for about me tabs and changed the html for that section

--- js/about-me-tabs.js
+++ js/about-me-tabs.js
@@ -1,16 +1,15 @@
 
-    const tablinks = document.querySelectorAll(".tab-links");
-    const tabcontents = document.querySelectorAll(".tab-contents");
+const tablinks = document.querySelectorAll(".tab-links");
+const tabcontents = document.querySelectorAll(".tab-contents");
 
-    function opentab(tabname) {
-    for (tablink of tablinks) {
-    tablink.classList.remove("active-link");
-}
-    for (tabcontent of tabcontents) {
-    tabcontent.classList.remove("active-tab");
-}
-    event.currentTarget.classList.add("active-link");
-    document.querySelector(tabname).classList.add("active-tab");
-}
+tablinks.forEach((tablink, index) => {
+    tablink.addEventListener("click", () => {
+        tablinks.forEach((tl) => tl.classList.remove("active-link"));
+        tabcontents.forEach((tc) => tc.classList.remove("active-tab"));
+        tablink.classList.add("active-link");
+        tabcontents[index].classList.add("active-tab");
+    });
+});
 
-
+tablinks[0].classList.add("active-link");
+tabcontents[0].classList.add("active-tab");
